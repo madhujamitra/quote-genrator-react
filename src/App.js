dee7c8cea4bc4f87e7d7caa509e0f89d8d79da77
madhujamitra <44411291+madhujamitra@users.noreply.github.com>
2023-10-06 12:47:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+
+
+
 
 function App() {
+  const [quote, setQuote] = useState("hello");
+  const [author, setAuthor] = useState("world");
+  //https://api.quotable.io/random
+
+  useEffect(() => {
+
+    fetch("https://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        ((quote) => {
+          setAuthor(quote.author);
+          setQuote(quote.content);
+          console.log(quote);
+        })
+      )
+  }, [])
+
+  let fetchNewQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        ((quote) => {
+          setAuthor(quote.author);
+          setQuote(quote.content);
+          console.log(quote);
+        })
+      )
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="App" id="quote-box">
+      <div className='quote' >
+        <h2 id="text">{quote}</h2>
+        <small id="author"> - {author}</small>
+        <br></br>
+        {/* <a id="tweet-quote" className='icon'onClick={fetchNewQuote} ><FontAwesomeIcon icon={faTwitter} size="sm" /></a> */}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          id="tweet-quote"
+          className='icon'
+          href={`https://twitter.com/intent/tweet?text=${`"${quote}" - ${author}`}`}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <FontAwesomeIcon icon={faTwitter} />
         </a>
-      </header>
+      </div>
+      <br></br>
+      <button className='btn' onClick={fetchNewQuote} id="new-quote">genrate new quotes</button>
+
     </div>
   );
 }
